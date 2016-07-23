575086b63ce5fd47213590d6f7f34e4f3a2b63fd
nicojs <jansennico@gmail.com>
2016-07-24 00:51:39 +0200

    docs(index): Add intro to Stryker

--- src/fillViewModel.js
+++ src/fillViewModel.js
@@ -2,18 +2,28 @@
 module.exports = function (dest) {
   var menu = {
     'Home': '/',
-    'Quickstart': '/quickstart.html'
+    'Quickstart': '/quickstart.html',
+    'Plugins': '/plugins.html',
+    'Supported mutators': '/mutators.html'
   };
   var currentUrl = '/' + dest;
+  var currentTitle = 'Home';
   if (currentUrl === '/index.html') {
     currentUrl = '/';
+  }
+  
+  for(var i in menu){
+    if(menu[i] === currentUrl){
+      currentTitle = i;
+    }
   }
 
   return {
     debug: true,
     name: 'Stryker - The JavaScript mutation testing framework',
-    tagline: 'Measure the effectiveness of your JavaScript unit tests.',
+    tagline: 'Measure the effectiveness of JavaScript tests.',
     currentUrl: currentUrl,
-    menu: menu
+    currentTitle: currentTitle,
+    menu: menu,
   };
 };
