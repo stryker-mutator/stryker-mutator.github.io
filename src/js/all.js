0c9fba4da701a6d0329daee46f07a36d38c3e341
Nico Jansen <jansennico@gmail.com>
2018-09-01 13:19:53 +0200

    Make example on home page more readable

--- src/js/all.js
+++ src/js/all.js
@@ -1,4 +1,6 @@
 import hljs from 'highlight.js/lib/highlight';
 import javascript from 'highlight.js/lib/languages/javascript';
+import shell from 'highlight.js/lib/languages/shell';
 hljs.registerLanguage('javascript', javascript);
+hljs.registerLanguage('shell', shell);
 hljs.initHighlightingOnLoad();
