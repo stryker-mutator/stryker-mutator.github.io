9d69966219e89500ba28330b6903df04fa707002
Bartosz Leoniak <bartosz.leoniak@wp.pl>
2018-09-07 10:16:07 +0200

    improve audit score (#86)
    * increase readability
    * add language
    * manifest update

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,7 +54,7 @@ module.exports = function (grunt) {
         stats: !process.env.NODE_ENV || process.env.NODE_ENV === 'development'
       },
       prod: webpackConfig,
-      dev: Object.assign({}, webpackConfig, { mode: 'development' })
+      dev: { ...webpackConfig, mode: 'development' }
     },
 
     connect: {
@@ -89,7 +89,7 @@ module.exports = function (grunt) {
         options: {
           data: require('./src/fillViewModel.js'),
           filters: { // Add markdown-it-named-headers as plugin
-            'markdown-it': function (text, options) {
+            'markdown-it': function (text) {
               return md.render(text, {
                 plugins: ['markdown-it-named-headers'],
                 html: true
