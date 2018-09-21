712483d3a58c25c8c70bb94e41178a3ac40b13d0
Nico Jansen <jansennico@gmail.com>
2018-09-22 00:43:55 +0200

    Remove more generated code

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 module.exports = {
   entry: './src/js/all.js',
   output: {
-    path: path.resolve(__dirname, 'root', 'dist'),
+    path: path.resolve(__dirname, 'generated-root', 'js'),
     filename: 'all.bundle.js'
   },
   mode: 'production'
