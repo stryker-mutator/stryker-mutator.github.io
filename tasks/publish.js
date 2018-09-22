d0316fa12e4a0561dd38e42fc178a6fc32c86c03
Nico Jansen <jansennico@gmail.com>
2018-09-22 21:50:58 +0200

    Make a start with the publish script

--- tasks/publish.js
+++ tasks/publish.js
@@ -1,3 +1,12 @@
 const { execSync } = require('child_process');
+const { EOL } = require('os');
 
-execSync('git checkout master');
+const sh = command => execSync(command, { encoding: 'utf8' });
+
+
+const gitStatus = sh('git status --short');
+if (gitStatus) {
+  throw new Error(`Working directory is not clean${EOL}${gitStatus}`);
+}
+
+// execSync('git checkout master');
