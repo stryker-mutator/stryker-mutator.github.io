11c77505d0c15302bf04060a37fca000e5d8ab94
Legopiraat <gijs.venne@hotmail.com>
2018-09-25 21:54:17 +0200

    Activate scala code for highlightjs

--- src/js/all.js
+++ src/js/all.js
@@ -1,9 +1,11 @@
 import hljs from 'highlight.js/lib/highlight';
 import javascript from 'highlight.js/lib/languages/javascript';
 import shell from 'highlight.js/lib/languages/shell';
+import scala from 'highlight.js/lib/languages/scala';
 import { register } from './navbar-toggle';
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('shell', shell);
+hljs.registerLanguage('scala', scala);
 hljs.initHighlightingOnLoad();
 register();
