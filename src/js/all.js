26fa4da865a924eb74bcc6aac555dc29dee62139
Nico Jansen <jansennico@gmail.com>
2019-04-03 09:50:58 +0200

    Add blog: mutation testing elements (#130)

--- src/js/all.js
+++ src/js/all.js
@@ -2,10 +2,12 @@ import hljs from 'highlight.js/lib/highlight';
 import javascript from 'highlight.js/lib/languages/javascript';
 import shell from 'highlight.js/lib/languages/shell';
 import scala from 'highlight.js/lib/languages/scala';
+import xml from 'highlight.js/lib/languages/xml';
 import { register } from './navbar-toggle';
 
 hljs.registerLanguage('javascript', javascript);
 hljs.registerLanguage('shell', shell);
 hljs.registerLanguage('scala', scala);
+hljs.registerLanguage('html', xml);
 hljs.initHighlightingOnLoad();
 register();
