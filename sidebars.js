3dbba32bb89e9ec8c5147fd9a88de1f4f361c340
Nico Jansen <jansennico@gmail.com>
2020-11-13 08:25:05 +0100

    Allow for root docs

--- sidebars.js
+++ sidebars.js
@@ -1,17 +1,19 @@
 // @ts-check
 module.exports = {
-  General: [
-    'General/introduction',
-    'General/example',
-    'General/dashboard',
-    'General/faq',
-  ],
-  // @ts-expect-error
-  'Mutation Testing': require('./docs/mutation-testing-elements/sidebar'),
-  // @ts-expect-error
-  'Stryker': require('./docs/stryker/sidebar'),
-  // @ts-expect-error
-  'Stryker.NET': require('./docs/stryker-net/sidebar'),
-  // @ts-expect-error
-  'Stryker4s': require('./docs/stryker4s/sidebar'),
+  docs: {
+    General: [
+      'General/introduction',
+      'General/example',
+      'General/dashboard',
+      'General/faq',
+    ],
+    // @ts-expect-error
+    'Mutation Testing': require('./docs/mutation-testing-elements/sidebar'),
+    // @ts-expect-error
+    'Stryker': require('./docs/stryker/sidebar'),
+    // @ts-expect-error
+    'Stryker.NET': require('./docs/stryker-net/sidebar'),
+    // @ts-expect-error
+    'Stryker4s': require('./docs/stryker4s/sidebar'),
+  }
 }
