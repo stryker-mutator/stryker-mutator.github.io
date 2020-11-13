7daa25697bb3ea8ec59b38fdbbe1f93086d20eca
Nico Jansen <jansennico@gmail.com>
2020-11-13 19:49:01 +0100

    Lint sidebars

--- sidebars.js
+++ sidebars.js
@@ -1,19 +1,14 @@
 // @ts-check
 module.exports = {
   docs: {
-    General: [
-      'General/introduction',
-      'General/example',
-      'General/dashboard',
-      'General/faq',
-    ],
+    General: ['General/introduction', 'General/example', 'General/dashboard', 'General/faq'],
     // @ts-expect-error
     'Mutation Testing': require('./docs/mutation-testing-elements/sidebar'),
     // @ts-expect-error
-    'Stryker': require('./docs/stryker/sidebar'),
+    Stryker: require('./docs/stryker/sidebar'),
     // @ts-expect-error
     'Stryker.NET': require('./docs/stryker-net/sidebar'),
     // @ts-expect-error
-    'Stryker4s': require('./docs/stryker4s/sidebar'),
-  }
-}
+    Stryker4s: require('./docs/stryker4s/sidebar'),
+  },
+};
