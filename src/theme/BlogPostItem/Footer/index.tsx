0ee681c7b6e04781ab8f47ec7048f01ac1a4d191
Hugo van Rijswijk <git@hugovr.nl>
2022-10-14 09:00:24 +0000

    Add blog comment section with Giscus (#465)
    Closes #119
    
    Adds a comment section to each blog using [Giscus](https://giscus.app/) as suggested by @rouke-broersma. This is a static site comment system that uses GitHub discussions for storage, which works quite well.
    
    It works by [_swizzling_](https://docusaurus.io/docs/swizzling) the blog footer component, by adding the `Giscus` component if the page is a blog page (and not the blog overview page). It works in both dark and light mode and looks like this:
    
    ![image](https://user-images.githubusercontent.com/10114577/195133659-3f8fb860-4584-4f15-be90-82e507e49de1.png)

---
+++ src/theme/BlogPostItem/Footer/index.tsx
@@ -0,0 +1,42 @@
+import { useColorMode } from '@docusaurus/theme-common';
+// @ts-expect-error exports do not work for some reason
+import { useBlogPost } from '@docusaurus/theme-common/internal';
+import type { WrapperProps } from '@docusaurus/types';
+import Giscus from '@giscus/react';
+import Footer from '@theme-original/BlogPostItem/Footer';
+import type FooterType from '@theme/BlogPostItem/Footer';
+import React from 'react';
+
+type Props = WrapperProps<typeof FooterType>;
+
+export default function FooterWrapper(props: Props): JSX.Element {
+  // Current page is a blog post page, or the blog list overview page
+  const { isBlogPostPage } = useBlogPost();
+
+  if (!isBlogPostPage) {
+    return <Footer {...props} />;
+  }
+
+  const { colorMode } = useColorMode();
+
+  return (
+    <>
+      <Footer {...props} />
+
+      <Giscus
+        repo="stryker-mutator/stryker-mutator.github.io"
+        repoId="MDEwOlJlcG9zaXRvcnk1OTE3NzI0Nw=="
+        category="Blog posts"
+        categoryId="DIC_kwDOA4b5H84CR7Bj"
+        mapping="title"
+        strict="0"
+        reactionsEnabled="1"
+        emitMetadata="0"
+        inputPosition="top"
+        theme={colorMode}
+        lang="en"
+        loading="lazy"
+      />
+    </>
+  );
+}
