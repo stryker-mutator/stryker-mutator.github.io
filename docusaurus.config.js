0707ca536ac280cae864c94ce4997da461e4b1b7
Hugo van Rijswijk <git@hugovr.nl>
2022-12-13 21:02:28 +0100

    Make image sizes explicit on homepage (#514)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -154,6 +154,8 @@ const config = {
         alt: 'Info Support Logo',
         src: 'images/info-support.svg',
         href: 'https://infosupport.com',
+        width: 117,
+        height: 33,
       },
     },
     prism: {
