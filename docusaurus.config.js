3fa22769d15c5cd20d93a68ed83a71e0658ba98d
Nico Jansen <jansennico@gmail.com>
2023-07-05 23:07:53 +0200

    Include announcement

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -36,7 +36,7 @@ const config = {
     announcementBar: {
       id: 'talk', // Any value that will identify this message.
       content:
-        '📽️ <a target="_blank" rel="noopener noreferrer" href="https://www.youtube.com/watch?v=sGwfwtkaDfk">Watch the video "How to test your tests in .NET" from <i>Nick Chapsas</i></a>',
+        '📢 try out the new <a target="_blank" href="https://stryker-mutator.io/stryker-playground/">Stryker Playground right here in your browser!</a>',
       backgroundColor: 'var(--ifm-navbar-background-color, #fff)',
       textColor: 'var(--ifm-navbar-link-color, #000)',
       isCloseable: true,
